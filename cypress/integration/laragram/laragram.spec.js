9eaab56c8bc856e59edae19436ebf596fb3fcf99
ashleygraf101 <ashley.graf101@gmail.com>
2020-08-14 17:44:50 +1000

    moved login to beforeeach and logout to aftereach

--- cypress/integration/laragram/laragram.spec.js
+++ cypress/integration/laragram/laragram.spec.js
@@ -10,38 +10,38 @@ describe('sanity check', () => {
 
 describe('Authorised user', () => {
 
+  // runs before each test in the block
+  beforeEach(() => {
+    cy.visit('/login')
+    cy.get('#email').type('demo@gmail.com')
+    cy.get('#password').type('123456')
+    cy.contains('Log In').click()
+    cy.visit('/')
+  })
+
+
+  afterEach(() => {
+    cy.contains('Demo').click()
+    cy.contains('Logout').click()
+    cy.contains('Laragram')
+  })
+
   //log in and log out
   it('Logs in and logs out', () => {
-      cy.visit('/login')
-      cy.get('#email').type('demo@gmail.com')
-      cy.get('#password').type('123456')
-      cy.contains('Log In').click()
-      cy.visit('/')
-      cy.contains('Demo').click()
-      cy.contains('Logout').click()
-      cy.contains('Laragram')
+     cy.contains('Laragram')
   })
 
   //log in, add file to feed, and log out
   it('Add photo to feed', () => {
-    cy.visit('/login')
-
-      cy.get('#email').type('demo@gmail.com')
-      cy.get('#password').type('123456')
-      cy.contains('Log In').click()
-      cy.visit('/')
-      cy.get('input[type="file"]').invoke('show')
-        .click({force: true})
-        .attachFile(yourFixturePath)
-        .click({force: true})
-      cy.contains('Next').click()
-      cy.get('.description-container')
-        .type("hello, is it me you're looking for?")
-      cy.contains('Share').click()
-      cy.visit('/')
-      cy.contains('Demo').click()
-      cy.contains('Logout').click()
-      cy.contains('Laragram')
+    cy.get('input[type="file"]').invoke('show')
+      .click({force: true})
+      .attachFile(yourFixturePath)
+      .click({force: true})
+    cy.contains('Next').click()
+    cy.get('.description-container')
+      .type("hello, is it me you're looking for?")
+    cy.contains('Share').click()
+    cy.visit('/')
   });
 });
 
